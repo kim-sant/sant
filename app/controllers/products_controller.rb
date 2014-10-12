9d2f2b2c2eae1c75fe44b565f5fa2921f52f4fd6
Bryon Finke <bryon.finke@gmail.com>
2014-10-12 15:35:04 -0500

    part two on image upload

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,8 @@
 class ProductsController < InheritedResources::Base
+  
+  def create
+    Product.create(params.require(:product).permit(:name, :price, :image))
+    redirect_to root_url
+  end
+  
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   storage :file
