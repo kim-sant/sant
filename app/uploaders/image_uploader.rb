e3f4b9af3631c429babd443554b57e5da3169a81
Bryon Finke <bryon.finke@gmail.com>
2014-10-21 23:58:18 -0500

    updates image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  storage :file
-  # storage :fog
+  if Rails.env.development?
+    storage :file
+  else
+    storage :fog
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
