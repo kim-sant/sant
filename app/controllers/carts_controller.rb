0caf05b51d07787451475457d6c06692e85dd5a0
Bryon Finke <bryon.finke@gmail.com>
2014-10-29 13:33:35 -0500

    bug

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -29,7 +29,7 @@ class CartsController < InheritedResources::Base
   
   def checkout
     if user_signed_in?
-      redirect_to delivery_info_url
+      redirect_to new_url_path
     else
       redirect_to new_user_session_path
     end
