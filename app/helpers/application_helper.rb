4bfe81107a2babebde6d720ea26f28c7b8caab2e
Bryon Finke <bryon.finke@gmail.com>
2015-01-14 15:25:58 -0600

    updates metadata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  def meta_keywords(tags = nil)
+    if tags.present?
+      content_for :meta_keywords, tags
+    else
+      content_for?(:meta_keywords) ? content_for(:meta_keywords) : "Sant, idrinksant, superfruit beverage, superfruit recipe, superfood, yoga, superfruit tea"
+    end
+  end
+
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : "The dried superfruit that transforms into a fresh beverage."
+    end
+  end
+  
 end
