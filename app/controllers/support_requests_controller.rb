b8ade805fe9aa0874ad8e25f1aa5e2227b46565d
Bryon Finke <bryon.finke@gmail.com>
2015-01-15 13:01:28 -0600

    update

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -1,5 +1,10 @@
 class SupportRequestsController < InheritedResources::Base
 
+  def create
+    Product.create(params.require(:product).permit(:name, :price, :image, :slug))
+    redirect_to root_url, notice: "Your support request has been received.  We will follow-up shortly."
+  end
+
   private
 
     def support_request_params
--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -1,2 +1,6 @@
 class SupportRequest < ActiveRecord::Base
+  
+  validates :email, presence: true
+  validates :message, presence: true
+  
 end
