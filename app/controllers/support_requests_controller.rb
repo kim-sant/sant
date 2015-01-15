b4bedfbb9afc1cb74ebb2fb08950192b0c00d131
Bryon Finke <bryon.finke@gmail.com>
2015-01-15 14:48:24 -0600

    update

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -1,14 +1,8 @@
 class SupportRequestsController < InheritedResources::Base
 
   def create
-    @support_request = SupportRequest.new(params[:support_request_params])
-
-    if @support_request.save
-      UserMailer.support_request(@support_request).deliver
-      redirect_to root_url, notice: "Your support request has been received.  We will follow-up shortly."
-    else
-      render action: "new"
-    end
+    SupportRequest.create(params.require(:support_request).permit(:name, :email, :message))
+    redirect_to root_url, notice: "Your support request has been received.  We will follow-up shortly."
   end
 
   private
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,11 @@ class UserMailer < ActionMailer::Base
   
   def registration(user)
     @user = user
-    mail(:to => "#{user.email} <#{user.email}>", :subject => "Registered")
+    mail(to: "#{user.email} <#{user.email}>", subject: "Registered")
   end
   
   def support_request(support_request)
     @request = support_request
-    mail(:to => "bryon.finke@gmail.com <bryon.finke@gmail.com>", :subject => "New support request")
+    mail(to: "bryon.finke@gmail.com <bryon.finke@gmail.com>", subject: "New support request")
   end
 end
