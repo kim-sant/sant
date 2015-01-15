d29b099cbcfbee18883149dcb63206881579b8ae
Bryon Finke <bryon.finke@gmail.com>
2015-01-15 14:51:54 -0600

    update

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -1,7 +1,8 @@
 class SupportRequestsController < InheritedResources::Base
 
   def create
-    SupportRequest.create(params.require(:support_request).permit(:name, :email, :message))
+    support_request = SupportRequest.create(params.require(:support_request).permit(:name, :email, :message))
+    UserMailer.support_request(support_request)
     redirect_to root_url, notice: "Your support request has been received.  We will follow-up shortly."
   end
 
