7add726ad5e4eb3c13798e088b671c5cca7df65b
Bryon Finke <bryon.finke@gmail.com>
2015-01-27 15:03:00 -0600

    aws environment variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.staging?
     config.fog_credentials = {
-      :provider               => "AWS",       # required
-      :aws_access_key_id      => "AKIAJZJ4JVIVKM6QILYA",       # required
-      :aws_secret_access_key  => "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",       # required
+      :provider               => "AWS",
+      :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
+      :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
     }
     config.fog_directory = "bryon-ecommerce"
     config.cache_dir = "#{Rails.root}/tmp/uploads"
