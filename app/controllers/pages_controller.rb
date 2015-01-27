c96689ace14cf0a4cac378f6b0ec4984aa466d03
Bryon Finke <bryon.finke@gmail.com>
2015-01-26 22:32:22 -0600

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < InheritedResources::Base
   
   def homepage
-    if session[:cart_id].present? && !Cart.find(session[:cart_id]).present?
-      session[:cart_id] = nil
-    end
+
   end
   
   def about
