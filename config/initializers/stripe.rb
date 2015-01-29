0a86be73d45dbf4cded2858f1a628c5bd8455e0a
Bryon Finke <bryon.finke@gmail.com>
2015-01-29 12:58:38 -0600

    update

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
-  Stripe.api_key = ENV["STRIPE_PUBLIC_KEY"]
-  STRIPE_PUBLIC_KEY = ENV["STRIPE_API_KEY"]
+  Stripe.api_key = ENV["STRIPE_API_KEY"]
+  STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"]
 else
   Stripe.api_key = ENV["STRIPE_PUBLIC_KEY_TEST"]
   STRIPE_PUBLIC_KEY = ENV["STRIPE_API_KEY_TEST"]
