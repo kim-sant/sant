c50f41a4cdb28f1ecab5d7a6e7219d8d341055ce
Bryon Finke <bryon.finke@gmail.com>
2015-01-31 11:07:55 -0600

    update

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -11,9 +11,9 @@ ActiveAdmin.register User do
       end
     end
     column("Email") {|user| link_to "#{user.email}", admin_user_path(user) }
-    column :current_sign_in_at
     column :last_sign_in_at
     column :sign_in_count
+    column :created_at
     actions
   end
   
