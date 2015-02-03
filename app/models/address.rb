fa2bdcc3f3ee4b77271b20b37433d3e756112fc3
Bryon Finke <bryon.finke@gmail.com>
2015-02-03 16:13:09 -0600

    update

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,6 +7,5 @@ class Address < ActiveRecord::Base
   validates :last_name, presence: true
   validates :line_1, presence: true
   validates :city, presence: true
-  validates :state, presence: true
   validates :zip, presence: true
 end
