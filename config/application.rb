65d8b984e0cd3a0f7119aaf8c8c712bf4ddda1fa
Josh Nixon <seriousfools@gmail.com>
2015-05-24 16:04:10 -0500

    rolling back

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,6 @@ module Sant
       end
     end
 
-    config.assets.compile = true
-    config.assets.precompile =  ['*.js', '*.css'] 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
