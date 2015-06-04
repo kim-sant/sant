5e21d3e67762ef478dc330c9253889604f824463
Josh Nixon <seriousfools@gmail.com>
2015-06-04 15:04:59 -0500

    take off ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :expire_hsts
   protect_from_forgery with: :exception
+
+
+private
+  def expire_hsts
+    response.headers["Strict-Transport-Security"] = 'max-age=0'
+  end
 end
